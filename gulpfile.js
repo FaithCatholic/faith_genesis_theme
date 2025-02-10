6d57732fcb0696c18db1b197ec9b193ea183b20b
Mike Jones <mjones@faithcatholic.com>
2025-02-10 14:01:19 -0500

    updated sass and some syntax

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,7 @@
 
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
-var importer = require('node-sass-globbing');
-var livereload = require('gulp-livereload')
+var livereload = require('gulp-livereload');
 var plumber = require('gulp-plumber');
 var sass = require('gulp-sass')(require('sass')); 
 var sourcemaps = require('gulp-sourcemaps');
@@ -11,57 +10,47 @@ var stripCssComments = require('gulp-strip-css-comments');
 var uglify = require('gulp-uglify');
 var googleWebFonts = require('gulp-google-webfonts');
 
-var sass_config = {
-  importer: importer,
-  includePaths: [
-    'node_modules/breakpoint-sass/stylesheets/',
-    'node_modules/singularitygs/stylesheets/',
-    'node_modules/modularscale-sass/stylesheets',
-    'node_modules/compass-mixins/lib/'
-  ]
-};
-
-// Google webfonts
+// Google Web Fonts
 gulp.task('fonts', function () {
   return gulp.src('./fonts.list')
     .pipe(googleWebFonts({}))
     .pipe(gulp.dest('./fonts'));
 });
 
+// Move Bootstrap Fonts
 gulp.task('glyphs', function() {
-   gulp.src('./bootstrap/assets/fonts/bootstrap/*.{ttf,woff,woff2,eot,eof,svg}')
-   .pipe(gulp.dest('./fonts/bootstrap'));
+  return gulp.src('./bootstrap/assets/fonts/bootstrap/*.{ttf,woff,woff2,eot,svg}')
+    .pipe(gulp.dest('./fonts/bootstrap'));
 });
 
-// Uglifies javascript
+// Minify JavaScript
 gulp.task('uglify', function() {
   return gulp.src('scripts/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('js'));
 });
 
-// Compiles sass
+// Compile SCSS
 gulp.task('sass', function () {
   return gulp.src('./scss/**/*.scss')
     .pipe(plumber())
     .pipe(sourcemaps.init())
-    .pipe(sass(sass_config).on('error', sass.logError))
+    .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
-      browsers: ['last 2 version']
+      overrideBrowserslist: ['last 2 versions'],
+      cascade: false
     }))
     .pipe(stripCssComments({preserve: false}))
-    // .pipe(cssmin())
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
+    .pipe(livereload());
 });
 
-// Type "gulp" in the cli to watch
+// Default Task (Watch Files)
 gulp.task('default', function(){
   livereload.listen();
-    gulp.watch('./scss/**/*.scss', gulp.series('sass'));
-    gulp.watch('./js/*.js', gulp.series('uglify'));
-    gulp.watch('./fonts.list', gulp.series('fonts'));
-    gulp.watch(['./css/style.css', './**/*.twig', './scripts/*.js'], function (files){
-      livereload.changed(files)
-    });
+  gulp.watch('./scss/**/*.scss', gulp.series('sass'));
+  gulp.watch('./js/*.js', gulp.series('uglify'));
+  gulp.watch('./fonts.list', gulp.series('fonts'));
+  gulp.watch(['./css/style.css', './**/*.twig', './scripts/*.js']).on('change', livereload.reload);
 });
